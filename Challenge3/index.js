159b747c504632e3d0904e3b4a7abc3d3f723843
iagoxxpro1 <yagoxx@hotmail.com>
2024-08-18 02:38:33 +0100

    update

--- Challenge3/index.js
+++ Challenge3/index.js
@@ -1,24 +1,28 @@
 
-class Heroi{
-    constructor(noem, idade, tipo){
-        this.nome = nome
-        this.idade = idade
-        this.tipo = tipo
+class Heroi {
+    constructor(nome, idade, tipo) {
+        this.nome = nome;
+        this.idade = idade;
+        this.tipo = tipo;
     }
 
-    atacar(){
-        let atacar
+    atacar() {
+        let ataque
 
-        if (this.tipo === `mago`){
-            ataque = `magia`
-        } else if (this.tipo === `guerreiro`){
-            ataque = `espada`
-        } else if (this.tipo === `monge`){
-            ataque = `artes marciais`
-        } else if (this.tipo === `ninja`){
-            ataque = `shuriken`
-        } 
-        console.log(`O ${this.type} atacou usando ${atack}`)
+        if (this.tipo === `mago`) {
+            ataque = `magia`;
+        } else if (this.tipo === `guerreiro`) {
+            ataque = `espada`;
+        } else if (this.tipo === `monge`) {
+            ataque = `artes marciais`;
+        } else if (this.tipo === `ninja`) {
+            ataque = `shuriken`;
+        } else {
+            ataque = `não atacou`
+        }
+        console.log(`O ${this.tipo} atacou usando ${ataque}`);
     } 
 }
 
+const heroi1 = new Heroi(`Sakura`, 17, `guerreiro`);
+heroi1.atacar();
