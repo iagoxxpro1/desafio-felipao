42ae424f9abce85036963881269dace0fb90f99f
iagoxxpro1 <yagoxx@hotmail.com>
2024-08-18 02:16:35 +0100

    desafio 3 do felipao

---
+++ Challenge3/index.js
@@ -0,0 +1,24 @@
+
+class Heroi{
+    constructor(noem, idade, tipo){
+        this.nome = nome
+        this.idade = idade
+        this.tipo = tipo
+    }
+
+    atacar(){
+        let atacar
+
+        if (this.tipo === `mago`){
+            ataque = `magia`
+        } else if (this.tipo === `guerreiro`){
+            ataque = `espada`
+        } else if (this.tipo === `monge`){
+            ataque = `artes marciais`
+        } else if (this.tipo === `ninja`){
+            ataque = `shuriken`
+        } 
+        console.log(`O ${this.type} atacou usando ${atack}`)
+    } 
+}
+
